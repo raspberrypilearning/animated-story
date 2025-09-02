9af7f14e60c9453a70d20a2c530ec114a84a5da9
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2025-09-02 15:33:53 +0100

    New translations scripts.js (Chinese Simplified)

---
+++ zh-CN/code/animated-story-complete/scripts.js
@@ -0,0 +1,20 @@
+// Hide bounce observer
+const bounceObserver = new IntersectionObserver((entries) => {
+  if (entries[0].isIntersecting) {
+    console.log('BOUNCE TRIGGER IN VIEWPORT');
+    document.querySelector('#bounce').style.opacity = 0;
+  }
+});
+bounceObserver.observe(document.querySelector('#hideBounce'));
+
+// Image observer
+const lazyImages = document.querySelectorAll('img');
+const imageObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      setTimeout(() => (entry.target.src = entry.target.getAttribute('data-src')), 1000);
+      imageObserver.unobserve(entry.target);
+    }
+  });
+});
+lazyImages.forEach((lazyImage) => imageObserver.observe(lazyImage));
