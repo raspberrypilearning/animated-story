e38e102305f3a795e19e97e796d5a78bb6ff8247
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2025-09-02 15:37:44 +0100

    New translations sammy.js (Chinese Simplified)

---
+++ zh-CN/code/animated-story-upgraded/sammy.js
@@ -0,0 +1,28 @@
+// Rising text observer
+const riseObserver = new IntersectionObserver((entries) => {
+  if (entries[0].isIntersecting) {
+    entries[0].target.classList.add('rise');
+    riseObserver.unobserve(entries[0].target);
+  }
+});
+riseObserver.observe(document.querySelector('p'));
+
+// Heading observer
+const headingObserver = new IntersectionObserver((entries) => {
+  document.querySelector('h1').classList.toggle('enabled', !entries[0].isIntersecting);
+});
+headingObserver.observe(document.querySelector('#headingTrigger'));
+
+// Snail observer
+const snailObserver = new IntersectionObserver(
+  (entries) => {
+    if (entries[0].isIntersecting) {
+      entries[0].target.classList.add('startCrawl');
+      snailObserver.unobserve(entries[0].target);
+    }
+  },
+  {
+    threshold: 1,
+  }
+);
+snailObserver.observe(document.querySelector('#snail'));
